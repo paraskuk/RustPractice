254db48de4f4e191e68a739460e2428ba9de68c1
Paraskevas Paraskevas <paraskevasp@gmail.com>
2025-04-10 15:56:12 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+
+
+
+fn average(arr: &[i32]) -> f64 {
+    //let arr = vec![1, 2, 3];
+    let sum = arr.iter().fold(0, |a, b| a + b);
+    let count = arr.iter().fold(0, |acc, _| acc + 1);
+    //let count = arr.len() as i32;
+
+    let average: f64 = sum as f64 / count as f64;
+
+    println!("The sum is {}", sum);
+    println!("The count is {}", count);
+    println!("The average is {}", average);
+    average
+}
+
+fn main() {
+    println!("Hello, world!");
+    let arr = vec![1, 2, 3];
+    average(&arr);
+
+
+}
