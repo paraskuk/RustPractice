baf49695bd9b07c0879e3f58ebc79b456ab9129e
Paraskevas Paraskevas <paraskevasp@gmail.com>
2025-04-10 16:00:05 +0100

    Merge pull request #1
    * refactor: streamline average calculation by using built-in methods

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,10 @@
 
 fn average(arr: &[i32]) -> f64 {
     //let arr = vec![1, 2, 3];
-    let sum = arr.iter().fold(0, |a, b| a + b);
-    let count = arr.iter().fold(0, |acc, _| acc + 1);
-    //let count = arr.len() as i32;
-    //let sum = arr.iter().sum::<i32>();
-
+    //let sum = arr.iter().fold(0, |a, b| a + b);
+    //let count = arr.iter().fold(0, |acc, _| acc + 1);
+    let count = arr.len() as i32;
+    let sum = arr.iter().sum::<i32>();
     let average: f64 = sum as f64 / count as f64;
 
     println!("The sum is {}", sum);
