1386fc92337d8e56e6d7bd59e38354a8b0d8ecb2
Paraskevas Paraskevas <paraskevasp@gmail.com>
2025-04-11 11:37:38 +0100

    refactor: add debug output for array items and indices

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ fn average(arr: &[i32]) -> f64 {
     //let arr = vec![1, 2, 3];
     //let sum = arr.iter().fold(0, |a, b| a + b);
     //let count = arr.iter().fold(0, |acc, _| acc + 1);
+    for (item, index) in arr.iter().enumerate() {
+        println!("{} : {}", item, index);
+    }
     let count = arr.len() as i32;
     let sum = arr.iter().sum::<i32>();
     let average: f64 = sum as f64 / count as f64;
@@ -16,7 +19,7 @@ fn average(arr: &[i32]) -> f64 {
 }
 
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
     let arr = vec![1, 2, 3];
     average(&arr);
 
